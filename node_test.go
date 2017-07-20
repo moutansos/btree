a59e130a7c1e5de7854a7adf38f112170ab44b8b
Ben Brougher <moutansos@gmail.com>
2017-07-20 07:30:35 -0700

    add test for insert

--- node_test.go
+++ node_test.go
@@ -12,3 +12,14 @@ func TestNewNode(t *testing.T) {
 	}
 }
 
+func TestInsertUint64at(t *testing.T) {
+	ara := []uint64{23, 45, 56, 78, 9}
+	ara = insertUint64at(ara, 1, 67)
+	if ara[1] != 67 {
+		t.Error("Invalid value at the insertion point")
+	} else if ara[0] != 23 {
+		t.Error("Invalid value at position 0")
+	} else if ara[2] != 45 {
+		t.Error("Invalid value at position 2")
+	}
+}
