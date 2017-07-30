6f0a35490280e71a80db249374ab5e8e2f2b5bf8
ben <moutansos@gmail.com>
2017-07-29 19:26:50 -0700

    add read node function

--- btree.go
+++ btree.go
@@ -3,4 +3,5 @@ package btree
 type BTree interface {
 	NewNode() (n *Node, err error)
 	WriteNode(n *Node) error
+	ReadNode(address int64) (n *Node, err error)
 }
--- diskbtree.go
+++ diskbtree.go
@@ -1,6 +1,8 @@
 package btree
 
 import (
+	"bytes"
+	"encoding/binary"
 	"os"
 )
 
@@ -20,7 +22,7 @@ func (t *BTreeOnDisk) WriteNode(n *Node) error {
 		return err
 	}
 
-	f, err := os.OpenFile(t.File, os.O_RDWR, 0666) //TODO: Open for writing
+	f, err := os.OpenFile(t.File, os.O_RDWR, 0666)
 	if os.IsNotExist(err) {
 		f, err = os.Create(t.File)
 	}
@@ -32,6 +34,40 @@ func (t *BTreeOnDisk) WriteNode(n *Node) error {
 
 	_, err = f.WriteAt(data, n.Address)
 	return err
+}
+
+func (t *BTreeOnDisk) ReadNode(address int64) (n *Node, err error) {
+	f, err := os.Open(t.File)
+	if os.IsNotExist(err) {
+		return nil, err
+	}
+	defer f.Close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	data := make([]byte, 504)
+
+	//TODO: Validate address
+	_, err = f.ReadAt(data, address)
+	if err != nil {
+		return nil, err
+	}
+
+	buf := bytes.NewBuffer(data)
+	bn := new(binaryNode)
+
+	err = binary.Write(buf, binary.LittleEndian, bn)
+	if err != nil {
+		return nil, err
+	}
+
+	n = new(Node)
+	n.Pointers = bn.Pointers
+	n.Data = bn.Data
+	n.Address = address
+	return n, nil
 }
 
 func (t *BTreeOnDisk) NewNode() (n *Node, err error) {
