eccaad8b0d2f720781926fd48d38f550807615c1
Ben Brougher <Ben Brougher>
2017-07-30 22:42:38 -0700

    add initial test for reading of nodes form disktree. still need debugging

--- diskbtree_test.go
+++ diskbtree_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestNewBTreeOnDisk(t *testing.T) {
 	dir := os.TempDir()
-	f := path.Join(dir, "btree.bin")
+	f := path.Join(dir, "test-new-btree.bin")
 	//f := "btree.bin"
 	tree, err := NewBTreeOnDisk(f)
 	if tree.File != f {
@@ -20,7 +20,7 @@ func TestNewBTreeOnDisk(t *testing.T) {
 
 func TestWriteNode(t *testing.T) {
 	dir := os.TempDir()
-	f := path.Join(dir, "btree-write.bin")
+	f := path.Join(dir, "test-btree-write.bin")
 	//f = "btree-write.bin"
 	dtree, err := NewBTreeOnDisk(f)
 	if err != nil {
@@ -41,5 +41,44 @@ func TestWriteNode(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 		return
+	}
+}
+
+func TestReadNode(t *testing.T) {
+	//f := path.Join(os.TempDir(), "test-read-node.go")
+	f := "test-read-node.bin"
+
+	//Create test data in the tree
+	tree, err := NewBTreeOnDisk(f)
+	if err != nil {
+		t.Error(err)
+	}
+	n, err := tree.NewNode()
+	if err != nil {
+		t.Error(err)
+	}
+
+	n.Data[0] = 332
+	n.Pointers[0] = 1
+	n.Pointers[1] = 2
+
+	n.Address = 0
+	err = n.Write()
+	if err != nil {
+		t.Error(err)
+	}
+
+	//Read and check input
+	rn, err := tree.ReadNode(0)
+	if err != nil {
+		t.Error(err)
+	} else if rn.Address != 0 {
+		t.Errorf("Invalid address %v given by the read function. Expected 0", rn.Address)
+	} else if rn.Data[0] != 332 {
+		t.Errorf("Invalid data %v given by the read function at index 0. Expected 332", rn.Data[0])
+	} else if rn.Pointers[0] != 1 {
+		t.Errorf("Invalid pointer %v given by the read function. Expected 1", rn.Pointers[0])
+	} else if rn.Pointers[1] != 2 {
+		t.Errorf("Invalid pointer %v given by the read function. Expected 2", rn.Pointers[1])
 	}
 }
